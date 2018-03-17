14e4fb9234a715f290dc8f5649fdfa5fdf98d1fc
Adrian Marinica <apmarinica@gmail.com>
2018-03-17 21:50:38 +0200

    Added unit testing

---
+++ pizza-factory/test/factory.test.js
@@ -0,0 +1,8 @@
+describe('Pizza Factory', function () {
+
+    it('should check if pizza and order are available', function () {
+        expect(pizza).toBeDefined();
+        expect(order).toBeDefined();
+    });
+
+});
